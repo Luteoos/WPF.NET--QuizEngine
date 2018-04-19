2b3a0a6813f1c5f62638964009e2b1250cadd873
Luteoos <lmati97@gmail.com>
2018-04-19 13:21:57 +0200

    180419
    added begining of DataModel
    TODO
    seal Data Model
    and everythung else
    and MVVM light

---
+++ QuizEngine/DataModel/QuizDataJson.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace QuizEngine.DataModel
+{
+    class QuizDataJson
+    {
+       
+        private List<string> Question;
+        //Dictionary<string,bool>[] TableAnswers;
+        private List<Dictionary<string, bool>> TableAnswers;
+
+        public QuizDataJson()
+        {
+
+        }
+        public List<string> GetQuestions()
+        {
+            
+            return this.Question;
+        }
+        public List<Dictionary<string,bool>>GetAnswers()
+        {
+            return this.TableAnswers;
+        }
+        /*tableosfic[0] = new Dictionary<string, bool>();
+            tableosfic[1] = new Dictionary<string, bool>();
+
+            bool a=tableosfic[0]["a"];
+
+
+            eh = new Dictionary<string, bool>();
+            QuestionNumber=eh.Count();
+            bool a=eh[eh.Keys.ElementAt(1)];
+            */
+        /* public void InitializeData(int QNumber)
+         {
+             //QuestionNumber = QNumber;
+             Question= new string[QNumber];
+             //TableAnswers = new Dictionary<string, bool>[QNumber];
+
+
+
+         }*/
+
+
+    }
+}
---
+++ QuizEngine/View/ViewMainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace QuizEngine.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ViewMainWindow.xaml
+    /// </summary>
+    public partial class ViewMainWindow : Window
+    {
+        public ViewMainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+    }
+}
