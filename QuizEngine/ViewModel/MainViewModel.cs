f40a0fe3f7ed529f556ca95d3e06c3c8fe995537
Mateusz Lutecki <lmati97@gmail.com>
2018-07-20 22:30:38 +0200

    Update MainViewModel.cs

--- QuizEngine/ViewModel/MainViewModel.cs
+++ QuizEngine/ViewModel/MainViewModel.cs
@@ -24,13 +24,13 @@ namespace QuizEngine.ViewModel
 
         public MainViewModel()
         {
-            Model=new ModelDataInput();
-            _answers=new ObservableCollection<string>();
+            Model = new ModelDataInput();
+            _answers = new ObservableCollection<string>();
             _boolAnswers = new ObservableCollection<bool>();
 
             InitTextFill();
             this._saveToFile = new RelayCommand(SaveToFile,true);
-            this._nextQuestion=new RelayCommand(NextQuestion);
+            this._nextQuestion = new RelayCommand(NextQuestion);
         }
 #region Text Fillers
         private void InitTextFill()
@@ -62,7 +62,7 @@ namespace QuizEngine.ViewModel
                 Error = "Can't leave default values!";
                 return false;
             }
-            if (_answers.Count!= _answers.Distinct().Count())
+            if (_answers.Count != _answers.Distinct().Count())
             {
                 Error = "Answers must be diffrent";
                 return false;
